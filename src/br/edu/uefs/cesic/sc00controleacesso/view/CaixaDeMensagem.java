324a895a945a949dbebca1495634e668656a71d8
roselmanendes <roselma.mendes@gmail.com>
2014-02-09 22:27:12 -0300

    adicionada classe da caixa de dialogo e colocado link para referencia.

--- src/br/edu/uefs/cesic/sc00controleacesso/view/CaixaDeMensagem.java
+++ src/br/edu/uefs/cesic/sc00controleacesso/view/CaixaDeMensagem.java
@@ -1,5 +1,5 @@
 package br.edu.uefs.cesic.sc00controleacesso.view;
-
+//http://developer.android.com/guide/topics/ui/dialogs.html
 import android.app.DialogFragment;
 
 public class CaixaDeMensagem extends DialogFragment {
